7df2691734b70b5d24c69095e8dcac02fb26b42f
Radu Matei <matei.radu94@gmail.com>
2016-12-28 21:28:46 +0200

    Added another message handler
    - added test message handler that maps “/test”

---
+++ samples/EchoTest/TestMessageHandler.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketManager;
+
+namespace EchoApp
+{
+    public class TestMessageHandler : WebSocketHandler
+    {
+        public TestMessageHandler(WebSocketManager.WebSocketManager webSocketManager) : base(webSocketManager)
+        {
+        }
+
+        public override async Task OnConnected(WebSocket socket)
+        {
+            await base.OnConnected(socket);
+
+            var socketId = WebSocketManager.GetId(socket);
+            await SendMessageToAllAsync($"{socketId} is now connected");
+        }
+
+        public override async Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            var socketId = WebSocketManager.GetId(socket);
+            var message = $"{socketId} said: {Encoding.UTF8.GetString(buffer, 0, result.Count)}";
+
+            await SendMessageToAllAsync(message);
+        }
+    }
+}
