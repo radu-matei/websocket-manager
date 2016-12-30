2d1a33d7dea4e499d46c53527ee8695b3c7c1b14
Radu Matei <matei.radu94@gmail.com>
2016-12-30 15:25:25 +0200

    Added controller for messages

---
+++ samples/MvcSample/Controllers/MessagesController.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcSample.MessageHandlers;
+
+namespace MvcSample.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private NotificationsMessageHandler _notificationsMessageHandler { get; set; }
+
+        public MessagesController(NotificationsMessageHandler notificationsMessageHandler)
+        {
+            _notificationsMessageHandler = notificationsMessageHandler;
+        }
+
+        [HttpGet]
+        public async Task SendMessage([FromQueryAttribute]string message)
+        {
+           await _notificationsMessageHandler.SendMessageToAllAsync(message);
+        }
+    }
+}
