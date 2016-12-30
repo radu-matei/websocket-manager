d5d9f09ad6b3135a9e12ad7324884b86c39cfb88
Radu Matei <matei.radu94@gmail.com>
2016-12-30 15:25:00 +0200

    Made web application

--- samples/MvcSample/Program.cs
+++ samples/MvcSample/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ConsoleApplication
+namespace MvcSample
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
