75861e1d7aeb75f02c23a86a8b5824ccbb262523
Radu Matei <matei.radu94@gmail.com>
2016-12-30 15:25:17 +0200

    Added message handler for notifications

---
+++ samples/MvcSample/MessageHandlers/NotificationsMessageHandler.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using WebSocketManager;
+
+namespace MvcSample.MessageHandlers
+{
+    public class NotificationsMessageHandler : WebSocketHandler
+    {
+        public NotificationsMessageHandler(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
+        {
+        }
+
+        public override Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
