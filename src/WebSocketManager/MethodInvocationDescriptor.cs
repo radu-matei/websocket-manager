4a28caa6544872573ce2dec28e9e6bd091d6fe92
Radu Matei <matei.radu94@gmail.com>
2017-01-10 16:42:17 +0200

    Added MethodInvocationDescriptor class

---
+++ src/WebSocketManager/MethodInvocationDescriptor.cs
@@ -0,0 +1,8 @@
+namespace WebSocketManager
+{
+    public class MethodInvocationDescriptor
+    {
+        public string MethodName { get; set; }
+        public object[] Arguments { get; set; }
+    }
+}
