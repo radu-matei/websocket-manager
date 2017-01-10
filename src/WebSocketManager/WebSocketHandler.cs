a8bcc889ed25ffb1d33674406d823d25e1b71a77
Radu Matei <matei.radu94@gmail.com>
2017-01-10 16:42:32 +0200

    Updated WebSocketHandler to invoke client methods

--- src/WebSocketManager/WebSocketHandler.cs
+++ src/WebSocketManager/WebSocketHandler.cs
@@ -3,13 +3,19 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace WebSocketManager
 {
     public abstract class WebSocketHandler
     {
         protected WebSocketConnectionManager WebSocketConnectionManager { get; set; }
-
+        
+        private JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
         public WebSocketHandler(WebSocketConnectionManager webSocketConnectionManager)
         {
             WebSocketConnectionManager = webSocketConnectionManager;
@@ -18,6 +24,11 @@ namespace WebSocketManager
         public virtual async Task OnConnected(WebSocket socket)
         {
             WebSocketConnectionManager.AddSocket(socket);
+
+            await SendMessageAsync(socket, new Message(){
+                MessageType = MessageType.ConnectionEvent,
+                Data = WebSocketConnectionManager.GetId(socket)
+            });
         }
 
         public virtual async Task OnDisconnected(WebSocket socket)
@@ -25,6 +36,59 @@ namespace WebSocketManager
             await WebSocketConnectionManager.RemoveSocket(WebSocketConnectionManager.GetId(socket));
         }
 
+        public async Task SendMessageAsync(WebSocket socket, Message message)
+        {
+            if(socket.State != WebSocketState.Open)
+                return;
+
+            var serializedMessage = JsonConvert.SerializeObject(message, _jsonSerializerSettings);
+            await socket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.ASCII.GetBytes(serializedMessage),
+                                                                  offset: 0, 
+                                                                  count: serializedMessage.Length),
+                                   messageType: WebSocketMessageType.Text,
+                                   endOfMessage: true,
+                                   cancellationToken: CancellationToken.None);  
+        }
+
+        public async Task SendMessageAsync(string socketId, Message message)
+        {
+            await SendMessageAsync(WebSocketConnectionManager.GetSocketById(socketId), message);
+        }
+
+        public async Task SendMessageToAllAsync(Message message)
+        {
+            foreach(var pair in WebSocketConnectionManager.GetAll())
+            {
+                if(pair.Value.State == WebSocketState.Open)
+                    await SendMessageAsync(pair.Value, message);
+            }
+        }
+
+        public async Task InvokeClientMethodAsync(string socketId, string methodName, object[] arguments)
+        {
+            var message = new Message()
+            {
+                MessageType = MessageType.ClientMethodInvocation,
+                Data = JsonConvert.SerializeObject(new MethodInvocationDescriptor()
+                {
+                    MethodName = methodName,
+                    Arguments = arguments
+                }, _jsonSerializerSettings)
+            };
+
+            await SendMessageAsync(socketId, message);
+        }
+
+        public async Task InvokeClientMethodToAllAsync(string methodName, params object[] arguments)
+        {
+            foreach(var pair in WebSocketConnectionManager.GetAll())
+            {
+                if(pair.Value.State == WebSocketState.Open)
+                    await InvokeClientMethodAsync(pair.Key, methodName, arguments);
+            }            
+        }
+
+/*
         public async Task SendMessageAsync(WebSocket socket, string message)
         {
             if(socket.State != WebSocketState.Open)
@@ -51,6 +115,8 @@ namespace WebSocketManager
                     await SendMessageAsync(pair.Value, message);
             }
         }
+*/
+
 
         //TODO - decide if exposing the message string is better than exposing the result and buffer
         public abstract Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer);
