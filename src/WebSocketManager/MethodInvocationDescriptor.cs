d9c0a7480fe94811038728e58a44819c48ab567f
Radu Matei <matei.radu94@gmail.com>
2017-01-10 20:41:29 +0200

    Renamed MethodInvocationDescriptor to InvocationDescriptor

--- src/WebSocketManager/MethodInvocationDescriptor.cs
+++
@@ -1,8 +0,0 @@
-namespace WebSocketManager
-{
-    public class MethodInvocationDescriptor
-    {
-        public string MethodName { get; set; }
-        public object[] Arguments { get; set; }
-    }
-}
