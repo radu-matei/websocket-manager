73ad7bda6c29af6f3731bd7d8ff0741c165fd4b4
Radu Matei <matei.radu94@gmail.com>
2017-01-10 20:41:54 +0200

    Handled non-existent server method invocation

--- src/WebSocketManager/WebSocketHandler.cs
+++ src/WebSocketManager/WebSocketHandler.cs
@@ -3,17 +3,16 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
 using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace WebSocketManager
 {
     public abstract class WebSocketHandler
     {
         protected WebSocketConnectionManager WebSocketConnectionManager { get; set; }
-        
+
         private JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings()
         {
             ContractResolver = new CamelCasePropertyNamesContractResolver()
@@ -27,7 +26,8 @@ namespace WebSocketManager
         {
             WebSocketConnectionManager.AddSocket(socket);
 
-            await SendMessageAsync(socket, new Message(){
+            await SendMessageAsync(socket, new Message()
+            {
                 MessageType = MessageType.ConnectionEvent,
                 Data = WebSocketConnectionManager.GetId(socket)
             });
@@ -40,16 +40,16 @@ namespace WebSocketManager
 
         public async Task SendMessageAsync(WebSocket socket, Message message)
         {
-            if(socket.State != WebSocketState.Open)
+            if (socket.State != WebSocketState.Open)
                 return;
 
             var serializedMessage = JsonConvert.SerializeObject(message, _jsonSerializerSettings);
             await socket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.ASCII.GetBytes(serializedMessage),
-                                                                  offset: 0, 
+                                                                  offset: 0,
                                                                   count: serializedMessage.Length),
                                    messageType: WebSocketMessageType.Text,
                                    endOfMessage: true,
-                                   cancellationToken: CancellationToken.None);  
+                                   cancellationToken: CancellationToken.None);
         }
 
         public async Task SendMessageAsync(string socketId, Message message)
@@ -59,9 +59,9 @@ namespace WebSocketManager
 
         public async Task SendMessageToAllAsync(Message message)
         {
-            foreach(var pair in WebSocketConnectionManager.GetAll())
+            foreach (var pair in WebSocketConnectionManager.GetAll())
             {
-                if(pair.Value.State == WebSocketState.Open)
+                if (pair.Value.State == WebSocketState.Open)
                     await SendMessageAsync(pair.Value, message);
             }
         }
@@ -83,63 +83,40 @@ namespace WebSocketManager
 
         public async Task InvokeClientMethodToAllAsync(string methodName, params object[] arguments)
         {
-            foreach(var pair in WebSocketConnectionManager.GetAll())
+            foreach (var pair in WebSocketConnectionManager.GetAll())
             {
-                if(pair.Value.State == WebSocketState.Open)
+                if (pair.Value.State == WebSocketState.Open)
                     await InvokeClientMethodAsync(pair.Key, methodName, arguments);
-            }            
-        }
-
-/*
-        public async Task SendMessageAsync(WebSocket socket, string message)
-        {
-            if(socket.State != WebSocketState.Open)
-                return;
-
-            await socket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.ASCII.GetBytes(message),
-                                                                  offset: 0, 
-                                                                  count: message.Length),
-                                   messageType: WebSocketMessageType.Text,
-                                   endOfMessage: true,
-                                   cancellationToken: CancellationToken.None);          
-        }
-
-        public async Task SendMessageAsync(string socketId, string message)
-        {
-            await SendMessageAsync(WebSocketConnectionManager.GetSocketById(socketId), message);
-        }
-
-        public async Task SendMessageToAllAsync(string message)
-        {
-            foreach(var pair in WebSocketConnectionManager.GetAll())
-            {
-                if(pair.Value.State == WebSocketState.Open)
-                    await SendMessageAsync(pair.Value, message);
             }
         }
-*/
 
-
-        //TODO - decide if exposing the message string is better than exposing the result and buffer
         public async Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
         {
             var serializedInvocationDescriptor = Encoding.UTF8.GetString(buffer, 0, result.Count);
-            var invocationDescriptor = JsonConvert.DeserializeObject<InvocationDescriptor>(serializedInvocationDescriptor, _jsonSerializerSettings);
+            var invocationDescriptor = JsonConvert.DeserializeObject<InvocationDescriptor>(serializedInvocationDescriptor);
 
             var method = this.GetType().GetMethod(invocationDescriptor.MethodName);
 
-            if(method == null)
+            if (method == null)
+            {
+                await SendMessageAsync(socket, new Message()
+                {
+                    MessageType = MessageType.Text,
+                    Data = $"Cannot find method {invocationDescriptor.MethodName}"
+                });
                 return;
+            }
+
             try
             {
                 method.Invoke(this, invocationDescriptor.Arguments);
             }
             catch (TargetParameterCountException e)
             {
-               await SendMessageAsync(socket, new Message()
+                await SendMessageAsync(socket, new Message()
                 {
-                   MessageType = MessageType.Text,
-                   Data = $"The {invocationDescriptor.MethodName} method does not take {invocationDescriptor.Arguments.Length} parameters!" 
+                    MessageType = MessageType.Text,
+                    Data = $"The {invocationDescriptor.MethodName} method does not take {invocationDescriptor.Arguments.Length} parameters!"
                 });
             }
 
