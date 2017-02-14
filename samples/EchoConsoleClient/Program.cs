5771ea83fdc6e75fc817d765ecfc28658bb6ba1c
Radu Matei <matei.radu94@gmail.com>
2017-02-15 00:05:46 +0200

    Reacted to the addition of start and stop connection methods

--- samples/EchoConsoleClient/Program.cs
+++ samples/EchoConsoleClient/Program.cs
@@ -3,15 +3,22 @@ using System.Threading.Tasks;
 using WebSocketManager.Client;
 public class Program 
 {
+    private static Connection _connection;
     public static void Main(string[] args)
     {
         StartConnectionAsync();
         Console.ReadLine();
+        StopConnectionAsync();
     }
 
     public static async Task StartConnectionAsync()
     {
-        var connection = new Connection();
-        await connection.StartConnectionAsync("ws://localhost:5000/test");
+        _connection = new Connection();
+        await _connection.StartConnectionAsync("ws://localhost:5000/test");
+    }
+
+    public static async Task StopConnectionAsync()
+    {
+        await _connection.StopConnectionAsync();
     }
 }
