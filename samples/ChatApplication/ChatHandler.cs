21f0b7215d11bd3371986a393895366e8274a98c
Rob Schoenaker <rob@schoenaker.eu>
2017-05-17 14:25:33 +0200

    Fixed error in filter value. 24 is not the same as "24"

--- samples/ChatApplication/ChatHandler.cs
+++ samples/ChatApplication/ChatHandler.cs
@@ -6,44 +6,45 @@ using WebSocketManager.Common;
 
 namespace ChatApplication
 {
-    public class ChatHandler : WebSocketHandler
+  public class ChatHandler : WebSocketHandler
+  {
+    public ChatHandler(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
     {
-        public ChatHandler(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
-        {
-        }
-
-        public override async Task OnConnected(WebSocket socket, HttpContext context)
-        {
-            await base.OnConnected(socket, context);
-
-            var socketId = WebSocketConnectionManager.GetId(socket);
-
-            var message = new Message()
-            {
-                MessageType = MessageType.Text,
-                Data = $"{socketId} is now connected"
-            };
-
-            await SendMessageToAllAsync(message, x => x.Query["age"].Equals(24));
-        }
-
-        public async Task SendMessage(string socketId, string message)
-        {
-            await InvokeClientMethodToAllAsync("receiveMessage", socketId, message);
-        }
-
-        public override async Task OnDisconnected(WebSocket socket)
-        {
-            var socketId = WebSocketConnectionManager.GetId(socket);
-
-            await base.OnDisconnected(socket);
-
-            var message = new Message()
-            {
-                MessageType = MessageType.Text,
-                Data = $"{socketId} disconnected"
-            };
-            await SendMessageToAllAsync(message, x => x.Query["age"].Equals(24));
     }
+
+    public override async Task OnConnected(WebSocket socket, HttpContext context)
+    {
+      await base.OnConnected(socket, context);
+
+      var socketId = WebSocketConnectionManager.GetId(socket);
+
+      var message = new Message()
+      {
+        MessageType = MessageType.Text,
+        Data = $"{socketId} is now connected"
+      };
+
+      await SendMessageToAllAsync(message, x => x.Query["age"] == "24");
+
     }
+
+    public async Task SendMessage(string socketId, string message)
+    {
+      await InvokeClientMethodToAllAsync("receiveMessage", socketId, message);
+    }
+
+    public override async Task OnDisconnected(WebSocket socket)
+    {
+      var socketId = WebSocketConnectionManager.GetId(socket);
+
+      await base.OnDisconnected(socket);
+
+      var message = new Message()
+      {
+        MessageType = MessageType.Text,
+        Data = $"{socketId} disconnected"
+      };
+      await SendMessageToAllAsync(message, x => x.Query["age"] == "24");
+    }
+  }
 }
