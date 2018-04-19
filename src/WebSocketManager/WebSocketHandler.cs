eb682ab4175b9dfa64e45b86df1c564a0d059c55
Henry de Jongh <henry00@live.nl>
2018-04-19 11:29:35 +0200

    Added parameterless InvokeClientMethodAsync and InvokeClientMethodToAllAsync.

--- src/WebSocketManager/WebSocketHandler.cs
+++ src/WebSocketManager/WebSocketHandler.cs
@@ -180,6 +180,8 @@ namespace WebSocketManager
             }
         }
 		
+        public async Task InvokeClientMethodAsync(string socketId, string method) => await InvokeClientMethodAsync(socketId, method, new object[] { });
+		
         public async Task InvokeClientMethodAsync<T1>(string socketId, string method, T1 arg1) => await InvokeClientMethodAsync(socketId, method, new object[] { arg1 });
 
         public async Task InvokeClientMethodAsync<T1, T2>(string socketId, string method, T1 arg1, T2 arg2) => await InvokeClientMethodAsync(socketId, method, new object[] { arg1, arg2 });
@@ -212,6 +214,8 @@ namespace WebSocketManager
 
         public async Task InvokeClientMethodAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(string socketId, string method, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16) => await InvokeClientMethodAsync(socketId, method, new object[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16 });
 
+        public async Task InvokeClientMethodToAllAsync(string method) => await InvokeClientMethodToAllAsync(method, new object[] { });
+		
         public async Task InvokeClientMethodToAllAsync<T1>(string method, T1 arg1) => await InvokeClientMethodToAllAsync(method, new object[] { arg1 });
 
         public async Task InvokeClientMethodToAllAsync<T1, T2>(string method, T1 arg1, T2 arg2) => await InvokeClientMethodToAllAsync(method, new object[] { arg1, arg2 });
